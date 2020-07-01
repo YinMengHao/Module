d9f9c579d559877b073b4dc9a461eef479a3e61e
MH <729263080@qq.com>
2020-07-01 13:40:30 +0800

    delete example

--- Example/Pods/Target Support Files/Module/Module-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Module : NSObject
-@end
-@implementation PodsDummy_Module
-@end
--- Example/Pods/Target Support Files/Pods-Module_Example/Pods-Module_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Module_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Module_Example
-@end
